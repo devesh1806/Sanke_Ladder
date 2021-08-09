1fce1f4a6ed409028d47e3fc46fef03ea6ed87b6
Devesh Bhangale <42887196+devesh1806@users.noreply.github.com>
2021-08-09 18:05:08 +0530

    [Devesh]: Added Number of Times Dice rolled and print position feature.

--- src/com/bridgelabz/snake_ladder/Snakeladder.java
+++ src/com/bridgelabz/snake_ladder/Snakeladder.java
@@ -10,14 +10,13 @@ public class Snakeladder {
 		
 		int player_pos=0; //start position of player is initialised with 0.
 		
-		int ran_num = (int)((Math.random()*10) % 6) + 1 ; //Rolling of dice implemented by using random function generating random numbers (1-6). 
-		
 		int times_roll = 0;  //variable for number of times dice rolled.  
 		
 		while ( player_pos < 100 ) {  //condition for player till reaching winning mark 100.
 			
 			int option_num = (int)((Math.random()*10) % 3); //Option number generated for next move between snake, ladder and no play.
-			
+			int ran_num = (int)((Math.random()*10) % 6) + 1 ; //Rolling of dice implemented by using random function generating random numbers (1-6). 
+		
 			switch(option_num) {
 			case no_play:         //case for move regarding no play
 				break;  
