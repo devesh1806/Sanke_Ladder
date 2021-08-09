8a7501188f5b0d2838a5af1f3d74d7b8a9b2a8e0
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-09 16:28:07 +0530

    [Devesh]: Added Player with its position initialised as zero

--- src/com/bridgelabz/snake_ladder/Snakeladder.java
+++ src/com/bridgelabz/snake_ladder/Snakeladder.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.snake_ladder;
 
 public class Snakeladder {
+	
 	public static void main(String[] args) {
 		
+		int player_pos=0; //start position of player is initialised with 0.
 		
+		System.out.println("The position of Player is "+ player_pos);
 	}
 }
