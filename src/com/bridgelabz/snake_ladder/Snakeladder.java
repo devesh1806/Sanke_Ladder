faf8b04f4bf4bbd6a7eef9fcdca1630a7dcde99b
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-09 16:39:47 +0530

    [Devesh]: Added Rolling of Dice feature for number between 1 to 6.

--- src/com/bridgelabz/snake_ladder/Snakeladder.java
+++ src/com/bridgelabz/snake_ladder/Snakeladder.java
@@ -6,6 +6,9 @@ public class Snakeladder {
 		
 		int player_pos=0; //start position of player is initialised with 0.
 		
-		System.out.println("The position of Player is "+ player_pos);
+		int ran_num = (int)((Math.random()*10) % 6) + 1 ; //Rolling of dice implemented by using random function generating random numbers (1-6). 
+		
+		System.out.println("The position of Player is " + player_pos);
+		System.out.println("After rolling of die the number is " + ran_num);
 	}
 }
