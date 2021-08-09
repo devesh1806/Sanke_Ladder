432a9c08a1b15ca520f8250bfc8bfa19b79aec93
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-09 16:59:25 +0530

    [Devesh]: Added Check Option feature which gives next move

--- src/com/bridgelabz/snake_ladder/Snakeladder.java
+++ src/com/bridgelabz/snake_ladder/Snakeladder.java
@@ -2,13 +2,29 @@ package com.bridgelabz.snake_ladder;
 
 public class Snakeladder {
 	
+	public static final int no_play = 0; //variable for no play
+	public static final int ladder = 1; //variable for ladder
+	public static final int snake = 2; //variable for snake
+	
 	public static void main(String[] args) {
 		
 		int player_pos=0; //start position of player is initialised with 0.
 		
 		int ran_num = (int)((Math.random()*10) % 6) + 1 ; //Rolling of dice implemented by using random function generating random numbers (1-6). 
 		
-		System.out.println("The position of Player is " + player_pos);
-		System.out.println("After rolling of die the number is " + ran_num);
+		int option_num = (int)((Math.random()*10) % 3); //Option number generated for next move between snake, ladder and no play.
+
+		switch(option_num) {
+		case no_play:         //case for move regarding no play
+			break;  
+		case ladder:         //case for move regarding ladder   
+			player_pos += ran_num; 
+			break;
+		case snake:        //case for move regarding snake
+			player_pos -= ran_num; 
+			break;
+		}
+		
+		System.out.println("The current position of Player is " + player_pos);
 	}
 }
